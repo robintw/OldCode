2a2e08ce7c424b5b4ba9c37ab6e3f66e631fe45a
r.t.wilson@rmplc.co.uk <r.t.wilson@rmplc.co.uk@ac94bd0b-b54e-0410-bc68-ad1411bb6d8f>
2009-02-08 09:19:35 +0000

    Added TestWPF solution.

---
+++ TestWPF/TestWPF/Results.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace TestWPF
+{
+    class Results : ObservableCollection<Result>
+    {
+        public Results()
+        {
+            this.Add(new Result("Cooking.pdf", @"D:\Users\Robin Wilson\Documents\Cooking.pdf"));
+            this.Add(new Result("BOCP Scan.png", @"D:\Users\Robin Wilson\Documents\BOCP Scan.png"));
+        }
+
+        public void Add(string _name, string _path)
+        {
+            this.Add(new Result(_name, _path));
+        }
+    }
+}
