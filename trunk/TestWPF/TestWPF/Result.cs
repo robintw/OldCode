4d7d04f3d3aaf268e7af7a73e68b77d84e7e901f
r.t.wilson@rmplc.co.uk <r.t.wilson@rmplc.co.uk@ac94bd0b-b54e-0410-bc68-ad1411bb6d8f>
2009-02-08 09:19:35 +0000

    Added TestWPF solution.

---
+++ trunk/TestWPF/TestWPF/Result.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
+namespace TestWPF
+{
+    class Result
+    {
+        private System.Drawing.Icon DisplayIcon;
+
+        public Result(string _name, string _path)
+        {
+            Name = _name;
+            Path = _path;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        public System.Drawing.Icon Icon
+        {
+            set
+            {
+                DisplayIcon = value;
+            }
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get
+            {
+                System.Drawing.Icon ico = System.Drawing.Icon.ExtractAssociatedIcon(Path);
+                Bitmap bmp = ico.ToBitmap();
+
+                MemoryStream strm = new MemoryStream();
+                bmp.Save(strm, System.Drawing.Imaging.ImageFormat.Png);
+                strm.Seek(0, SeekOrigin.Begin);
+
+                PngBitmapDecoder pbd = new PngBitmapDecoder(strm, BitmapCreateOptions.None, BitmapCacheOption.Default);
+
+                return pbd.Frames[0];
+            }
+        }
+
+        public void Run()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = Path;
+            proc.Start();
+        }
+
+    }
+}
---
+++ trunk/TestWPF/TestWPF/Window1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private Results SearchResults;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            SearchResults.Add("HTCR.pdf", @"D:\Users\Robin Wilson\Documents\University\HTCR.pdf");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SearchResults = new Results();
+            listBox1.ItemsSource = SearchResults;
+            comboBox1.ItemsSource = SearchResults;
+            comboBox1.Focus();
+
+            string[] files = Directory.GetFiles(@"C:\Users\Robin Wilson\AppData\Roaming\Microsoft\Windows\Start Menu", "*.lnk", SearchOption.AllDirectories);
+            foreach (string filename in files)
+            {
+                FileInfo fi = new FileInfo(filename);
+
+                SearchResults.Add(new Result(fi.Name, fi.FullName));
+            }
+
+            
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (e.AddedItems.Count > 0)
+            //{
+            //    Result SelRes = (Result)e.AddedItems[0];
+            //    MessageBox.Show(SelRes.Name);
+            //    //SelRes.Run();
+            //}
+
+        }
+
+        private void comboBox1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            comboBox1.IsDropDownOpen = true;
+        }
+
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                Result SelRes = (Result)comboBox1.SelectedItem;
+                //MessageBox.Show(SelRes.Name);
+                SelRes.Run();
+            }
+        }
+    }
+}
